a4ebe24219be02c52c6223b219d6797197241c9e
stassrv <stas.sirovui@gmailn.comgit>
2019-06-02 00:50:14 +0300

    contacts

--- js/main.js
+++ js/main.js
@@ -6,57 +6,59 @@
     menu.classList.toggle("open");
   });
 
+  
+
   //Add map
-  function initMap() {
-    //Create map and asign to this baMap var
-    let mapCenter = {
-      lat: 49.584661,
-      lng: 34.559750
-    };
-    let baMap = new google.maps.Map(document.getElementById('ba-map'), {
-    center: mapCenter,
-    zoom: 17
+  // function initMap() {
+  //   //Create map and asign to this baMap var
+  //   let mapCenter = {
+  //     lat: 49.584661,
+  //     lng: 34.559750
+  //   };
+  //   let baMap = new google.maps.Map(document.getElementById('ba-map'), {
+  //   center: mapCenter,
+  //   zoom: 17
     
-    });
+  //   });
 
-    
-
-    let cities = {
-    Кооперативная: {lat: 49.986794,lng: 36.232815},
-    Васильковская: {lat: 50.393865,lng: 30.489887},
-    Пушкинская: {lat:50.447584,lng:30.518842},
-    Конституции: {lat: 49.584661,lng: 34.559750}
-    };
-    let mapMarkers = {};
-
-    for(let city in cities){
-    
-    let marker = new google.maps.Marker(
-      {
-        position: cities[city],
-         map: baMap,
-         icon: 'img/marker.svg'	
-        }
-      );
-      let infowindow = new google.maps.InfoWindow({
-      content: '<b>' + city + '</b>'
-      });
-      infowindow.open(baMap, marker);
-
-      mapMarkers[city] = marker;
-    }
-    // The marker, positioned at Uluru
     
 
-    baMap.setCenter(mapCenter);
+  //   let cities = {
+  //   Кооперативная: {lat: 49.986794,lng: 36.232815},
+  //   Васильковская: {lat: 50.393865,lng: 30.489887},
+  //   Пушкинская: {lat:50.447584,lng:30.518842},
+  //   Конституции: {lat: 49.584661,lng: 34.559750}
+  //   };
+  //   let mapMarkers = {};
 
-    $('#city-select').on('change', function (e) {
-      baMap.panTo(cities[this.value]);
-    })
-  }
-  $(document).ready(function (e){
-    initMap();
-  });
+  //   for(let city in cities){
+    
+  //   let marker = new google.maps.Marker(
+  //     {
+  //       position: cities[city],
+  //        map: baMap,
+  //        icon: 'img/marker.svg'	
+  //       }
+  //     );
+  //     let infowindow = new google.maps.InfoWindow({
+  //     content: '<b>' + city + '</b>'
+  //     });
+  //     infowindow.open(baMap, marker);
+
+  //     mapMarkers[city] = marker;
+  //   }
+  //   // The marker, positioned at Uluru
+    
+
+  //   baMap.setCenter(mapCenter);
+
+  //   $('#city-select').on('change', function (e) {
+  //     baMap.panTo(cities[this.value]);
+  //   })
+  // }
+  // $(document).ready(function (e){
+  //   initMap();
+  // });
 
 })();
 
@@ -81,6 +83,19 @@ $(function() {
 		clickedContent.slideToggle();
 	});
 })(jQuery);
+
+;(function($) {
+	"use strict";
+	let cities = {
+      Кооперативная: {lat: 49.986794,lng: 36.232815},
+      Васильковская: {lat: 50.393865,lng: 30.489887},
+      Пушкинская: {lat:50.447584,lng:30.518842},
+      Конституции: {lat: 49.584661,lng: 34.559750}
+      };
+
+	
+})(jQuery);
+
 
 let menuSlider = $('.ba-menu-slider');
 
